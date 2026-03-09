da646239955e56f7af409f89a447f5505a49bae1
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-03-09 14:08:14 +0100

    Pinger and Ponger

--- lab-activities/lab-activity-04/pcd-lab-04/src/main/java/pcd/lab04/ex01_synchwithsem/Pinger.java
+++ lab-activities/lab-activity-04/pcd-lab-04/src/main/java/pcd/lab04/ex01_synchwithsem/Pinger.java
@@ -1,13 +1,27 @@
 package pcd.lab04.ex01_synchwithsem;
 
+import java.util.concurrent.Semaphore;
+
 public class Pinger extends ActiveComponent {
 
-	public Pinger() {
+	private final Semaphore pongDoneEvent;
+	private final Semaphore pingDoneEvent;
+
+	public Pinger(final Semaphore pongDoneEvent, final Semaphore pingDoneEvent) {
+		this.pongDoneEvent = pongDoneEvent;
+		this.pingDoneEvent = pingDoneEvent;
 	}	
 	
 	public void run() {
 		while (true) {
-			println("ping");
+            try {
+                this.pongDoneEvent.acquire();
+				println("ping");
+				this.pingDoneEvent.release();
+
+			} catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
 		}
 	}
 }
--- lab-activities/lab-activity-04/pcd-lab-04/src/main/java/pcd/lab04/ex01_synchwithsem/Ponger.java
+++ lab-activities/lab-activity-04/pcd-lab-04/src/main/java/pcd/lab04/ex01_synchwithsem/Ponger.java
@@ -1,13 +1,27 @@
 package pcd.lab04.ex01_synchwithsem;
 
+import java.util.concurrent.Semaphore;
+
 public class Ponger extends ActiveComponent {
+	private Semaphore pingDoneEvent;
+	private Semaphore pongDoneEvent;
+
 	
-	public Ponger() {
-	}	
+	public Ponger(final Semaphore pingDoneEvent, final Semaphore pongDoneEvent) {
+		this.pingDoneEvent = pingDoneEvent;
+		this.pongDoneEvent = pongDoneEvent;
+	}
 	
 	public void run() {
 		while (true) {
-			println("pong");
+            try {
+                this.pingDoneEvent.acquire();
+				println("pong");
+				this.pongDoneEvent.release();
+
+			} catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
 		}
 	}
 }
